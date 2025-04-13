f62fc5cd5025bc01988b5ae0ef56282d3413e0d5
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2025-04-14 00:41:48 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
-// server.js
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDb from "./config/db.js"; // Import connectDb
+import connectDb from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import dataRoutes from "./routes/data.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Load environment variables
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Create Express app
 const app = express();
 
 // Middleware
@@ -26,25 +27,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+// Serve React build
+app.use(express.static(path.join(__dirname, "client/build")));
+
 // Routes
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Welcome to the Save Gaza API");
 });
-
 app.use("/api/auth", authRoutes);
 app.use("/api/data", dataRoutes);
 
-// Error handling middleware
+// Serve React app for non-API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+// Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-// Start server and connect to MongoDB
+// Start server
 const PORT = process.env.PORT || 5100;
 const startServer = async () => {
   try {
-    await connectDb(); // Use connectDb
+    await connectDb();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
