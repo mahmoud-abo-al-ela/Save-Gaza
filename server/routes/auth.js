40fe0390f4034d7d58fb14d0774919676221c975
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2025-04-14 00:16:08 +0200

    fix issue

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
