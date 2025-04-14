6910aca4e57269fc825f95e069e8cb6654fda7fe
Mahmoud Ali <79024928+mahmoud-abo-al-ela@users.noreply.github.com>
2025-04-14 13:27:29 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = [
   process.env.CORS_ORIGIN,
   'http://localhost:3000',
-  'https://save-gaza-fehz.onrender.com', // Add frontend URL
+  'https://save-gaza.onrender.com', // Add frontend URL
 ].filter(Boolean);
 
 app.use(cors({
